8f94f0fb469dcf7ef7d82872bcef4bda12fc3197
Ajay Kammardi Suryanarayana <ajay.kammardi.suryanarayana@zalando.de>
2021-05-21 17:26:48 +0200

    Added loading scripts

--- airflow/plugins/helpers/sql_queries.py
+++ airflow/plugins/helpers/sql_queries.py
@@ -27,9 +27,9 @@ class SqlQueries:
                     id SERIAL,
                     organization_key varchar(50),
                     event_date date,
-                    user_created_count integer,
-                    user_updates_count integer,
-                    user_deleted_count integer 
+                    user_created varchar(250),
+                    user_updated varchar(250),
+                    user_deleted varchar(250) 
                 );
             """)
 
@@ -78,7 +78,7 @@ class SqlQueries:
                 );
             """)
 
-    target_user_dimension_table_create = ("""
+    target_user_dimension_history_table_create = ("""
                 CREATE TABLE IF NOT EXISTS public.dim_user_history (
                     id SERIAL,
                     username varchar(250),
@@ -88,3 +88,68 @@ class SqlQueries:
                     created_at timestamp              
                 );
             """)
+
+    data_validation_rule_org_name_missing = ("""
+            UPDATE public.staging_events
+            SET data_check_failed = 'Y'
+            WHERE organization_name IS NULL;
+        """)
+
+    data_validation_rule_user_name_missing = ("""
+                UPDATE public.staging_events
+                SET data_check_failed = 'Y'
+                WHERE username IS NULL;
+            """)
+
+    load_data_into_user_dimension_table = ("""
+            INSERT INTO public.dim_user
+            SELECT username,
+                    user_email,
+                    user_type,
+                    plan_name,
+                    received_at AS created_at
+            FROM public.staging_events
+            WHERE DATE(received_at) = {}
+    """)
+
+    load_data_into_user_history_dimension_table = ("""
+                INSERT INTO public.dim_user_history
+                SELECT public.dim_user.username,
+                    public.dim_user.user_email,
+                    public.dim_user.user_type,
+                    public.dim_user.plan_name,
+                    public.dim_user.created_at
+                FROM public.staging_events,
+                public.dim_user
+                WHERE public.staging_events.username = public.dim_user.username
+                AND DATE(received_at) = {}
+        """)
+
+    delete_data_from_user_dimension_table = ("""
+                    DELETE FROM public.dim_user
+                    WHERE public.dim_user.username IN (
+                    SELECT public.dim_user.username
+                    FROM public.staging_events,
+                    public.dim_user
+                    WHERE public.staging_events.username = public.dim_user.username
+                    AND DATE(public.staging_events.received_at) = {} )
+            """)
+
+    load_data_into_fact_table = ("""
+                    INSERT INTO public.fact_organization
+                    SELECT public.dim_organization.organization_key,
+                            DATE(public.staging_events.received_at) AS event_date,
+                            CASE
+                            WHEN public.staging_events.event_date = 'User Created' THEN public.staging_events.username
+                            END AS user_created,
+                            CASE
+                            WHEN public.staging_events.event_date = 'User Updated' THEN public.staging_events.username
+                            END AS user_updated,
+                            CASE
+                            WHEN public.staging_events.event_date = 'User Deleted' THEN public.staging_events.username
+                            END AS user_deleted
+                            FROM public.dim_organization,
+                                public.staging_events
+                    WHERE public.staging_events.organization_name = public.dim_organization.organization_name
+                    AND DATE(public.staging_events.received_at) = {};
+    """)
